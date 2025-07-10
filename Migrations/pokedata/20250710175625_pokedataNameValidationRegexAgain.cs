bd35432a3e02ef1f2b1b7445e822a7a865a9fdbb
K <greenk1@roehampton.ac.uk>
2025-07-10 18:56:41 +0100

    changed name filter validation again

---
+++ Migrations/pokedata/20250710175625_pokedataNameValidationRegexAgain.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pokedata.Migrations.pokedata
+{
+    /// <inheritdoc />
+    public partial class pokedataNameValidationRegexAgain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
