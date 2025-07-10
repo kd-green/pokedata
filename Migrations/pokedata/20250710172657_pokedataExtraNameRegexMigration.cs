ec8b7b481a6d30fcc14d1459bfcaee5b6c8b0560
K <greenk1@roehampton.ac.uk>
2025-07-10 18:30:21 +0100

    updated db based on new regex filtering for names

---
+++ Migrations/pokedata/20250710172657_pokedataExtraNameRegexMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pokedata.Migrations.pokedata
+{
+    /// <inheritdoc />
+    public partial class pokedataExtraNameRegexMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
