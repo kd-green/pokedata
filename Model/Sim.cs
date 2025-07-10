22e258793c072dd379a64280c98ef7450700d0e3
K <greenk1@roehampton.ac.uk>
2025-07-10 16:46:20 +0100

    added validation to model

--- Model/Sim.cs
+++ Model/Sim.cs
@@ -10,20 +10,35 @@
 
         public string? Title { get; set; }
 
+        [Required]
+        [StringLength(26)] // In line with HMRC name length limits
         public string FirstName { get; set; }
 
+        [Required]
+        [StringLength(26)]
         public string LastName { get; set; }
 
+        [Required]
+        [MaxLength(1)]
+        [RegularExpression(@"[MF]")]
         public char Sex { get; set; }
 
+        [Required]
         public DateOnly DateOfBirth { get; set; }
 
+        [Required]
+        [RegularExpression(@"[a-zA-z0-9'\s-]*")]
         public string AddressLine1 { get; set; }
 
+        [Required]
+        [RegularExpression(@"[a-zA-z0-9'\s-]*")]
         public string AddressLine2 { get; set; }
 
+        [Required]
+        [RegularExpression(@"([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})")]
         public string PostalCode { get; set; }
 
+        [Required]
         public HealthConditions HealthCondition { get; set; }
     }
 }
